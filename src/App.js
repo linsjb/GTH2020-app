fce6efce198d548853b6966c63036f4fdc03064b
Salinda Hettiarachchi <shettiarachchi@cinglevue.com>
2020-02-21 22:57:31 +0100

    Home view and the navigation part completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,103 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import 'react-native-gesture-handler';
+import React, {useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button, StyleSheet} from 'react-native';
+import Tabs from 'react-native-tabs';
 
 import HelloHack from './components/HelloHack';
 
+const Courses = ({navigation}) => {
+
+  const setState = (name) => {
+    navigation.navigate(name, {name: 'Jane'})
+  }
+
+  return (
+    <View style={styles.container}>
+      <Tabs selected={"first"} style={{backgroundColor:'white'}}
+              selectedStyle={{color:'red'}} onSelect={ el => setState(el.props.name)}>
+            <Text name="Courses">Courses</Text>
+            <Text name="Badges">Badges</Text>
+            <Text name="News">News</Text>            
+      </Tabs>
+      <Text>Courses</Text>
+    </View>    
+  );
+}
+
+const Badges = ({navigation}) => {
+
+  const setState = (name) => {
+    navigation.navigate(name, {name: 'Jane'})
+  }
+
+  return (
+    <View style={styles.container}>
+      <Tabs selected={"first"} style={{backgroundColor:'white'}}
+              selectedStyle={{color:'red'}} onSelect={ el => setState(el.props.name)}>
+            <Text name="Courses">Courses</Text>
+            <Text name="Badges">Badges</Text> 
+            <Text name="News">News</Text>           
+      </Tabs>
+      <Text>Badges</Text>
+    </View>    
+  );
+}
+
+const News = ({navigation}) => {
+
+  const setState = (name) => {
+    navigation.navigate(name, {name: 'Jane'})
+  }
+
+  return (
+    <View style={styles.container}>
+      <Tabs selected={"first"} style={{backgroundColor:'white'}}
+              selectedStyle={{color:'red'}} onSelect={ el => setState(el.props.name)}>
+            <Text name="Courses">Courses</Text>
+            <Text name="Badges">Badges</Text> 
+            <Text name="News">News</Text>           
+      </Tabs>
+      <Text>News</Text>
+    </View>    
+  );
+}
+
+const Stack = createStackNavigator();
+
 const App = () => {
   return (
-    <>
-      <HelloHack />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Courses" component={Courses} options={{title: 'Courses'}}/>
+        <Stack.Screen name="Badges" component={Badges} />
+        <Stack.Screen name="News" component={News} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
 
 export default App;
