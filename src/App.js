e4d867f5f5636f93af8ccc3cd5556b6005e3f7ef
Salinda Hettiarachchi <shettiarachchi@cinglevue.com>
2020-02-22 06:21:01 +0100

    Topic file integration

--- src/App.js
+++ src/App.js
@@ -8,16 +8,25 @@ import Header from './components/Header';
 import HelloHack from './components/HelloHack';
 import BadgesComponent from './components/Badges';
 import CoursesComponent from './components/Courses'
+import TopicComponent from './components/Topic'
 
 const Courses = ({navigation}) => {
   const setState = name => {
     navigation.navigate(name, {name: 'Jane'});
   };
 
+  const navigate = () => {    
+    navigation.navigate("Topics", {name: 'Jane'});
+  }
+
+  const doNothing = () => {    
+    
+  }
+
   return (
     <View style={styles.container}>
       <Header title='Mindist'/>       
-      <CoursesComponent></CoursesComponent>
+      <CoursesComponent callback={navigate} callbackEmpty={doNothing}></CoursesComponent>
       <Tabs selected={"first"} style={{backgroundColor:'white'}}
               selectedStyle={{color:'red'}} onSelect={ el => setState(el.props.name)}>
             <Text name="Courses" style={{color:'red'}} >Courses</Text>
@@ -80,6 +89,32 @@ const News = ({navigation}) => {
   );
 };
 
+
+const Topics = ({navigation}) => {
+  const setState = name => {
+    navigation.navigate(name, {name: 'Jane'});
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header title="Mindist" />
+      
+      <TopicComponent></TopicComponent>
+
+      <Tabs
+        selected={'first'}
+        style={{backgroundColor: 'white'}}
+        selectedStyle={{color: 'red'}}
+        onSelect={el => setState(el.props.name)}>
+        <Text name="Courses">Courses</Text>
+        <Text name="Badges">Badges</Text>
+        <Text name="News">News
+        </Text>
+      </Tabs>
+    </View>
+  );
+};
+
 const Stack = createStackNavigator();
 
 const App = () => {
@@ -93,6 +128,7 @@ const App = () => {
         />
         <Stack.Screen name="Badges" component={Badges} options={{title: ''}} />
         <Stack.Screen name="News" component={News} options={{title: ''}} />
+        <Stack.Screen name="Topics" component={Topics} options={{title: ''}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
